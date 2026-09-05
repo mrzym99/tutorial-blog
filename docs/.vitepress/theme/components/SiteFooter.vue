0054d868d074e0ed00f516dae5bbc97fd3760794
19983467897 <2715158815.com>
2026-09-05 23:55:26 +0800

    site: footer

--- docs/.vitepress/theme/components/SiteFooter.vue
+++ docs/.vitepress/theme/components/SiteFooter.vue
@@ -4,19 +4,20 @@ import { SITE_ICP } from "../site";
 
 const { site } = useData();
 const year = new Date().getFullYear();
+const href = `https://www.icpbacx.com/search/${SITE_ICP}`;
 </script>
 
 <template>
   <footer class="site-footer">
     <p class="copyright">© {{ year }} {{ site.title }}</p>
     <!-- ICP 备案要求：展示备案号并链接到工信部网站；未配置（如海外部署）不显示 -->
-    <a v-if="SITE_ICP" href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">{{ SITE_ICP }}</a>
+    <a v-if="SITE_ICP" :href="href" target="_blank" rel="noreferrer">{{ SITE_ICP }}</a>
   </footer>
 </template>
 
 <style scoped>
 .site-footer {
-  padding: 2rem 1.5rem 3rem;
+  padding: 2rem 1.5rem 2.5rem;
   text-align: center;
   font-size: 0.875rem;
   line-height: 1.75;
